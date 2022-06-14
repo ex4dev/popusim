cbec19c75f9745f545d5c29856177680f5213391
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-06-13 20:28:02 -0400

    Prevent negative numbers in NumberComponent

--- src/components/NumberComponent.tsx
+++ src/components/NumberComponent.tsx
@@ -2,19 +2,15 @@ import { Component, Setter } from "solid-js"
 
 import styles from "./../assets/styles/NumberComponent.module.css"
 
-export const NumberComponent: Component = (props: any | {number: number, setter: Setter<number>}) => {
+export const NumberComponent: Component = (props: any | { number: number, setter: Setter<number> }) => {
     return (
         <div>
             <button class={styles.button} onclick={() => {
                 props.setter(props.number + props.increment)
             }}>⬆</button>
-            <span style={{
-                display: "inline-block",
-                "min-width": "50px",
-                "text-align": "center"
-            }}>{Math.round(props.number * 1000) / 1000}</span>
+            <span class={styles.text}>{Math.round(props.number * 1000) / 1000}</span>
             <button class={styles.button} onclick={() => {
-                props.setter(props.number - props.increment)
+                props.setter(Math.max(0, props.number - props.increment))
             }}>⬇</button>
             <span>{props.name}</span>
         </div>
